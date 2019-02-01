050bc15bb325102ac16f51396d1b79dc1a42fc93
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-01 18:16:30 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -23,23 +23,17 @@ function booksReducer(state = [], action) {
   }
 }
 
+function authorsReducer(state = [], action) {
+  let idx;
+  switch (action.type) {
     case "ADD_AUTHOR":
-        return {
-          ...state,
-          authors: [...state.authors, action.author]
-        };
+        return [...state, action.author]
 
     case "REMOVE_AUTHOR":
       idx = state.authors.indexOf(action.id);
-      return {
-        ...state,
-        authors: [
-          state.authors.slice(0, idx),
-          state.authors.slice(idx + 1)
-        ]
-      };
+      return [state.slice(0, idx), state.slice(idx + 1)]
 
     default:
       return state;
-    }
-};
+  }
+}
